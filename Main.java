cca4b5176e14e76af7db3227dd449225a66480c9
sstuarrt <95904642+sstuarrt@users.noreply.github.com>
2022-06-12 20:59:27 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,22 +1,22 @@
-package com.company.java_core.homework1.task1;
+package com.company.java_core.homework1.task2;
 
 public class Main {
     public static void main(String[] args) {
-        int Int = 5;
-        double Double = 2435.56;
-        float Float = 2.5f;
-        byte Byte = 1;
-        short Short = 2;
-        long Long = 54567865;
-        char Char = 'a';
-        String string = "Hello!";
-        boolean Bool = true;
+        int[] array = new int[10];
 
-        System.out.println("byte = [-128; 127]");
-        System.out.println("short = [-32768; 32767]");
-        System.out.println("int = [-2147483648; 2147483647]");
-        System.out.println("long = [-9223372036854775808; 9223372036854775807]");
-        System.out.println("float = [~1,4 * 10^-45; ~3,4 * 10^38]");
-        System.out.println("double = [~4,9 * 10^-324; ~1,8 * 10^308]");
+        int min = 0;
+        int max = 0;
+
+        for(int i = 0; i < array.length; i++){
+            array[i] = ((int)(Math.random() * 31) - 15);
+            System.out.println(array[i]);
+            if(array[i] > max){
+                max = array[i];
+            } else if(array[i] < min){
+                min = array[i];
+            }
+        }
+        System.out.println("Minimal = " + min);
+        System.out.println("Maximal = " + max);
     }
 }
